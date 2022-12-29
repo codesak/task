218d7e82bea79ba4b96e725cf8de7b49f49f6a83
codesak <sakshamverma7844@gmail.com>
2022-12-30 01:06:34 +0530

    all done

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -15,13 +15,28 @@ const PersonalDetails = () => {
   const [firstName, setFirstName] = useState("")
   const [lastName, setLastName] = useState("")
   const [gender, setGender] = useState("")
+  const [date, setDate] = useState("")
+  const [email, setEmail] = useState("")
+  const [fatherName, setFatherName] = useState("")
+  const [motherName, setMotherName] = useState("")
+  const [address, setAddress] = useState("")
+  const [phoneNo, setPhoneNo] = useState();
+  const [maritialStatus, setMaritialStatus] = useState("")
+  const [education, setEducation] = useState("")
+  const [occupationType, setOccupationType] = useState("")
+  const [income, setIncome] = useState("")
+  const [panNo, setPanNo] = useState("")
+  const [aadhar, setAadhar] = useState("");
+
+
+
+
+
+
   const [err, setErr] = useState(false)
 
   const handleSubmit = ()=>{
-
-
-    
-    if(!firstName || !gender || !lastName){
+    if(!firstName || !lastName || !gender || !date || !email || !fatherName || !motherName || !address || !phoneNo || !maritialStatus || !education || !occupationType || !income || !panNo || !aadhar){
       setErr(true)
       return;
     }else{
@@ -34,14 +49,26 @@ const PersonalDetails = () => {
             {
                 firstName: firstName,
                 lastName: lastName,
-                gender:gender
+                gender:gender,
+                date:date,
+                email:email,
+                fatherName:fatherName,
+                motherName:motherName,
+                address:address,
+                phoneNo:phoneNo,
+                maritialStatus:maritialStatus,
+                education:education,
+                occupationType:occupationType,
+                income:income,
+                panNo:panNo,
+                aadhar:aadhar
             })
             .then(res => {
                 console.log(res.data)
                 if (res.data.code === 200) {
-                    alert(' success.')
+                    console.log("success");
                 } else {
-                    alert('Error.')
+                    console.log("err");
                 }
             }).catch(err => {
                 console.log(err)
@@ -77,24 +104,26 @@ const PersonalDetails = () => {
             <MenuItem value="Other">Other</MenuItem>
           </TextField>
 
-        <TextField placeholder='dd/mm/yyyy' label="Date of Birth" variant="outlined" style={{width:"8vw"}}>Date of Birth</TextField>
-        <TextField placeholder="Email" label="Email" variant="outlined" style={{width:"26vw"}}>Email</TextField>
+        <TextField value={date} onChange={(e)=>setDate(e.target.value)} placeholder='dd/mm/yyyy' label="Date of Birth" variant="outlined" style={{width:"8vw"}}>Date of Birth</TextField>
+        <TextField value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email" label="Email" variant="outlined" style={{width:"26vw"}}>Email</TextField>
         
         </div>
 
         <div className="details_formsection">
-        <TextField placeholder="Father's Full Name" label="Father's Full Name" variant="outlined" style={{width:"21vw"}}>Email</TextField>
-        <TextField placeholder="Mother's Full Name" label="Mother's Full Name" variant="outlined" style={{width:"21vw"}}>Email</TextField>
+        <TextField value={fatherName} onChange={(e)=>setFatherName(e.target.value)} placeholder="Father's Full Name" label="Father's Full Name" variant="outlined" style={{width:"21vw"}}>Email</TextField>
+        <TextField value={motherName} onChange={(e)=>setMotherName(e.target.value)} placeholder="Mother's Full Name" label="Mother's Full Name" variant="outlined" style={{width:"21vw"}}>Email</TextField>
         </div>
 
         <div className="details_formsection">
-          <TextField placeholder='Address' label="Address" variant='outlined' style={{width:"23vw"}}>Address</TextField>
-          <TextField placeholder="Phone Number" label="Phone Number" variant="outlined" style={{width:"9vw"}}>Phone Number</TextField>
+          <TextField value={address} onChange={(e)=>setAddress(e.target.value)} placeholder='Address' label="Address" variant='outlined' style={{width:"23vw"}}>Address</TextField>
+          <TextField value={phoneNo} onChange={(e)=>setPhoneNo(e.target.value)} placeholder="Phone Number" label="Phone Number" variant="outlined" style={{width:"9vw"}} type="number" >Phone Number</TextField>
           <TextField
           select
           variant='outlined'
           label="Marital Status"
           style={{width:"9vw"}}
+          value={maritialStatus}
+          onChange={(e)=>setMaritialStatus(e.target.value)}
          
           >
             <MenuItem value="Single">Single</MenuItem>
@@ -110,6 +139,8 @@ const PersonalDetails = () => {
           variant='outlined'
           label="Education"
           style={{width:"12vw"}}
+          value={education}
+          onChange={(e)=>setEducation(e.target.value)}
          
           >
             <MenuItem value="Under Graduate">Under Graduate</MenuItem>
@@ -122,6 +153,8 @@ const PersonalDetails = () => {
           variant='outlined'
           label="Occupation Type"
           style={{width:"13vw"}}
+          value={occupationType}
+          onChange={(e)=>setOccupationType(e.target.value)}
          
           >
             <MenuItem value="Salaried">Salaried</MenuItem>
@@ -133,6 +166,8 @@ const PersonalDetails = () => {
           variant='outlined'
           label="Annual Income"
           style={{width:"16vw"}}
+          value={income}
+          onChange={(e)=>setIncome(e.target.value)}
          
           >
             <MenuItem value="Less than 1 lakhs">Less than 1 lakhs </MenuItem>
@@ -145,8 +180,8 @@ const PersonalDetails = () => {
         </div>
 
         <div className="details_formsection">
-        <TextField placeholder="PAN Number" label="PAN Number" variant="outlined" style={{width:"16vw"}}>PAN Number</TextField>
-        <TextField placeholder="Aadhar Number" label="Aadhar Number" variant="outlined" style={{width:"26vw"}}>Aadhar Number</TextField>
+        <TextField value={panNo} onChange={(e)=>setPanNo(e.target.value)} placeholder="PAN Number" label="PAN Number" variant="outlined" style={{width:"16vw"}}>PAN Number</TextField>
+        <TextField value={aadhar} onChange={(e)=>setAadhar(e.target.value)} placeholder="Aadhar Number" label="Aadhar Number" variant="outlined" style={{width:"26vw"}} type="number">Aadhar Number</TextField>
         </div>
 
         <div className="details_formsection">
